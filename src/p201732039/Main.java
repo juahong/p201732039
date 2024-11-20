5b6a84400ebaa2e90fed152c2c23fb9b5d3ea445
JUA <hjua7@naver.com>
2024-11-20 09:25:41 +0900

    메시지 two

--- src/p201732039/Main.java
+++ src/p201732039/Main.java
@@ -1,7 +1,9 @@
 package p201732039;
 
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("201732039 홍주아");
-	}
+    public static void main(String[] args) {
+        System.out.println("201732039 홍주아");
+        System.out.println(Hello.sum(10));
+        System.out.println(Hello.sum(100));
+    }
 }
